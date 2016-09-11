cf7ec5a6af27a428ba58488848ea9f877eabcf5c
F <fwrizalman@gmail.com>
2016-09-11 20:40:53 +0800

    Showing photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,9 +6,14 @@ class PhotosController < ApplicationController
   def create
     @photo = Photo.new(photo_params)
     if @photo.save
+      redirect_to photo_path(@photo)
     else
       render :new
     end
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
   end
 
   private
