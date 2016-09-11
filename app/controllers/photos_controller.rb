19f21c58a73df21438734b7b169a73f14d0bfb07
F <fwrizalman@gmail.com>
2016-09-11 21:24:36 +0800

    Updating photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -20,6 +20,20 @@ class PhotosController < ApplicationController
     @photo = Photo.find(params[:id])
   end
 
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+
+    if @photo.update(photo_params)
+      redirect_to @photo
+    else
+      render :edit
+    end
+  end
+
   private
     def photo_params
       params.require(:photo).permit(:file, :caption)
